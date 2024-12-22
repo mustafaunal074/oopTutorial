eab537040301e49004b40e25e8ff5f634080bc35
Mustafa Ünal <mustafaunal074@gmail.com>
2024-12-22 21:36:08 +0300

    class memberslari incelendi

---
+++ Ders03_ClassMembers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Ders03_ClassMembers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            #region Class Members
+
+            #region Field
+
+            // Nesne içerisinde değer tutmamızı sağlayan alanlardır.
+
+            MyClass m1 = new MyClass();// Nesne oluşturduk !!!
+            System.Console.WriteLine(m1.a); 
+            m1.a = 5;
+            System.Console.WriteLine(m1.a);
+            MyClass m2 = new MyClass(); 
+            m2.a = 25;
+
+            #endregion
+            #region Property
+                
+            #endregion
+            #region Metot
+                
+            #endregion
+            #region Indexer
+                
+            #endregion
+                
+            #endregion
+        }
+    }
+
+    class MyClass
+    {
+        public int a; // field
+        public string b; // field
+    }
+}
---
+++ Ders03_ClassMembers/classMembers.md
@@ -0,0 +1,5 @@
+# Class Members
+
+## Field
+
+![field](../Ders00_Ekstralar/resimler/field.png)
