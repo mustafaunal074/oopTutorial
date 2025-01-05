cf7f8ed6a07a565ff9d8adbb2351c7e03795fef9
Mustafa Ünal <mustafaunal074@gmail.com>
2025-01-05 20:16:58 +0300

    deconstruct incelendi

---
+++ Ders13_Deconstruct/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ders13_Deconstruct
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Mustafa",
+                Age = 23
+            };
+            var(name,age) = person;
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Deconstruct(out string name,out int age)
+        {
+            name = Name;
+            age = Age;
+        }
+    }
+}
---
+++ Ders13_Deconstruct/deconstruct.md
@@ -0,0 +1,8 @@
+# Deconstruct
+
+## Deconstruct Metodu Nedir?
+
+* Bir sınıf içerisinde "Deconstruct" ismiyle tanımlanan metot, compiler tarafından özel olarak algılanmakta ve sınıfın nesnesi üzerinden geriye hızlıca Tuple bir değer döndürmemizi sağlamaktadır.
+* Diğer özel class memberları class ismiyle tanımlanırken Deconstruct kendi ismiyle tanımlanır.
+
+![deconstructPrototipi](../Ders00_Ekstralar/resimler/deconstructPrototipi.png)
